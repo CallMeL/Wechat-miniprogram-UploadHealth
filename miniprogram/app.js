51d78401394e3dba88abfea8d56c67b9bd314442
CallMeL <chenxi2.71@qq.com>
2022-04-12 16:14:56 +0800

    完善tabbar

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -2,6 +2,9 @@ import TimerState from './config/timerState'
 import { CLOUD_ENV_ID } from './config'
 
 App({
+  globalData:{
+    electedIndex: 0
+  },
   onLaunch() {
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
--- miniprogram/custom-tab-bar/index.js
+++ miniprogram/custom-tab-bar/index.js
@@ -1,11 +1,16 @@
+const app = getApp()
 Component({
   onShow(){
-    if (typeof this.getTabBar === 'function' &&
-      this.getTabBar()) {
+    if (typeof this.getTabBar === 'function' && this.getTabBar()) {
       this.getTabBar().setData({
-        selected: 2    // 根据tab的索引值设置
+        selected: 0   // 根据tab的索引值设置
       }) 
     }
+  },
+  ready:function(){
+    this.setData({
+      selected: app.globalData.selectedIndex
+    })
   },
   data: {
     selected: 0,
@@ -15,40 +20,38 @@ Component({
     "list": [
       {
         "current": 0,
-        "pagePath": "pages/home/index",
+        "pagePath": "/pages/home/index",
         "text": "主页",
         "iconPath": "../images/tab-bar/home.png",
         "selectedIconPath": "../images/tab-bar/home-selected.png"
       },
       {
         "current": 1,
-        "pagePath": "pages/detail/index",
+        "pagePath": "/pages/calendar/index",
         "text": "日历",
         "iconPath": "../images/tab-bar/calender.png",
-        "selectedIconPath": "/images/sui-selected.png"
+        "selectedIconPath": "../images/tab-bar/calender.png"
       },
       {
         "current": 2,
-        "pagePath": "pages/user/index",
+        "pagePath": "/pages/user/index",
         "text": "我的",
         "iconPath": "../images/tab-bar/user.png",
-        "selectedIconPath": "/images/board-selected.png"
+        "selectedIconPath": "../images/tab-bar/user.png"
       }
     ]
   },
   attached() {
   },
   methods: {
+
     switchTab(e) {
-      console.log(e);
-      const idx = e.currentTarget.dataset.index
-      const path = e.currentTarget.dataset.path
-      // this.setData({
-      //   selected: idx
-      // })
-      wx.switchTab({
-        url: path,
-      });
+      const data = e.currentTarget.dataset;
+      const url = data.path;
+      app.globalData.selectedIndex = data.index;
+      wx.switchTab({ 
+        url: url
+        })
     }
   }
 })
