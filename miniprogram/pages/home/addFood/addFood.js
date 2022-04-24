5b801e7f5b4f657ca2530506172ef0c00bcd49b7
CallMeL <chenxi2.71@qq.com>
2022-04-24 18:10:15 +0800

    选择食物来源

--- miniprogram/pages/home/addFood/addFood.js
+++ miniprogram/pages/home/addFood/addFood.js
@@ -11,7 +11,7 @@ Page({
     },
     //mindKeys:['米饭','牛奶','苹果','黄瓜'],
     keys:['米饭'],//自定义热门搜索     
-    tabs: ['收入', '支出', '通知','我的'],
+    tabs: ['系统食物','我的食物'],
     stv: {
       windowWidth: 0,
       lineWidth: 0,
@@ -36,68 +36,6 @@ Page({
       this.tabsCount = tabs.length;
     } catch (e) {
     }
-  },
-  handlerStart(e) {
-    let {clientX, clientY} = e.touches[0];
-    this.startX = clientX;
-    this.tapStartX = clientX;
-    this.tapStartY = clientY;
-    this.data.stv.tStart = true;
-    this.tapStartTime = e.timeStamp;
-    this.setData({stv: this.data.stv})
-  },
-  handlerMove(e) {
-    let {clientX, clientY} = e.touches[0];
-    let {stv} = this.data;
-    let offsetX = this.startX - clientX;
-    this.startX = clientX;
-    stv.offset += offsetX;
-    if(stv.offset <= 0) {
-      stv.offset = 0;
-    } else if(stv.offset >= stv.windowWidth*(this.tabsCount-1)) {
-      stv.offset = stv.windowWidth*(this.tabsCount-1);
-    }
-    this.setData({stv: stv});
-  },
-  handlerCancel(e) {
-
-  },
-  handlerEnd(e) {
-    let {clientX, clientY} = e.changedTouches[0];
-    let endTime = e.timeStamp;
-    let {tabs, stv, activeTab} = this.data;
-    let {offset, windowWidth} = stv;
-    //快速滑动
-    if(endTime - this.tapStartTime <= 300) {
-      //向左
-      if(Math.abs(this.tapStartY - clientY) < 50) {
-        if(this.tapStartX - clientX > 5) {
-          if(activeTab < this.tabsCount -1) {
-            this.setData({activeTab: ++activeTab})
-          }
-        } else {
-          if(activeTab > 0) {
-            this.setData({activeTab: --activeTab})
-          }
-        }
-        stv.offset = stv.windowWidth*activeTab;
-      } else {
-        //快速滑动 但是Y距离大于50 所以用户是左右滚动
-        let page = Math.round(offset/windowWidth);
-        if (activeTab != page) {
-          this.setData({activeTab: page})
-        }
-        stv.offset = stv.windowWidth*page;
-      }
-    } else {
-      let page = Math.round(offset/windowWidth);
-      if (activeTab != page) {
-        this.setData({activeTab: page})
-      }
-      stv.offset = stv.windowWidth*page;
-    }
-    stv.tStart = false;
-    this.setData({stv: this.data.stv})
   },
   _updateSelectedPage(page) {
     let {tabs, stv, activeTab} = this.data;
@@ -157,7 +95,7 @@ Page({
     }
     var queryBean = JSON.stringify(this.data.nowAddingFood)
     wx.navigateTo({
-      url: '../../home/food-detail/food-detail?queryBean='+ queryBean+ "&list=" + list,
+      url: '../../home/food-detail/food-detail?queryBean='+ queryBean+ "&list=" + list+ "&source=" + this.data.activeTab,
     })
   }
   
